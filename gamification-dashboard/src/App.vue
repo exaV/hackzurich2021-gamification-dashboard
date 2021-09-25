4fa3b7e538497a8a68b27fd356bfb2d11ec30ad3
Patrick Del Conte <patrick.delconte@ti8m.ch>
2021-09-25 11:40:43 +0200

    add TeamGoalWidget.vue

--- gamification-dashboard/src/App.vue
+++ gamification-dashboard/src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <div id="app"><link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet"/>
+  <div id="app">
+    <link
+      href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css"
+      rel="stylesheet"
+    />
     <v-app>
       <transition name="fade" appear>
-        <div class="modal-overlay" v-if="showModal" @click="showModal = false"></div>
+        <div
+          class="modal-overlay"
+          v-if="showModal"
+          @click="showModal = false"
+        ></div>
       </transition>
       <v-main>
         <v-container>
           <v-row>
             <v-col v-for="n in 16" :key="n" cols="4">
               <v-card elevation="2" height="300" width="400">
-                <random-chart></random-chart>
+                <random-chart v-if="n % 2 === 0"></random-chart>
+                <team-goal-widget v-if="n % 2 === 1"></team-goal-widget>
               </v-card>
             </v-col>
             <v-col cols="4">
               <v-card elevation="2" height="300" width="400" class="addBoard">
-                <div class="addBoardDiv" @click="showModal = true"><v-icon x-large dark class="hugeicon">mdi-plus-circle-outline</v-icon></div>
+                <div class="addBoardDiv" @click="showModal = true">
+                  <v-icon x-large dark class="hugeicon"
+                    >mdi-plus-circle-outline
+                  </v-icon>
+                </div>
               </v-card>
             </v-col>
           </v-row>
@@ -23,29 +36,31 @@
         <transition name="slide" appear>
           <div class="modal" v-if="showModal">
             <h1>Create a Leaderboard</h1>
-            <p>To compare and compete with others, you can create your own leaderboard, just specify a title, what type
-              the competition should have, and which interface to use. You can add a description, and define whether the
-              leaderboard is private or if anyone can compete.
+            <p>
+              To compare and compete with others, you can create your own
+              leaderboard, just specify a title, what type the competition
+              should have, and which interface to use. You can add a
+              description, and define whether the leaderboard is private or if
+              anyone can compete.
             </p>
-            <BoardForm v-on:closeModal="showModal =false"></BoardForm>
+            <BoardForm v-on:closeModal="showModal = false"></BoardForm>
           </div>
         </transition>
-
       </v-main>
     </v-app>
   </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import BarChartWidget from "@/components/BarChartWidget.vue";
 import RandomChart from "@/components/RandomChart.vue";
 import BoardForm from "@/components/BoardForm.vue";
-
-
+import TeamGoalWidget from "@/components/TeamGoalWidget.vue";
 
 @Component({
   components: {
+    TeamGoalWidget,
     RandomChart,
     BarChartWidget,
     BoardForm,
@@ -53,16 +68,15 @@ import BoardForm from "@/components/BoardForm.vue";
   data: () => {
     return {
       showModal: false,
-    }
+    };
   },
 })
-
-export default class App extends Vue {
-}
+export default class App extends Vue {}
 </script>
 
 <style>
 @import "./assets/styles/stylesheet.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ gamification-dashboard/src/components/TeamGoalWidget.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container style="display: inline-flex; max-height: 100%">
+    <v-row style="max-height: inherit; max-width: 100%">
+      <BarChart
+        :chartdata="sum"
+        :options="chartOptions"
+        :styles="chartStyles"
+        style="max-width: 30%; max-height: 100%"
+      ></BarChart>
+      <v-spacer style="width: 1em"></v-spacer>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Participant</th>
+              <th class="text-left">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in contributors.datasets" :key="item.label">
+              <td>{{ item.label }}</td>
+              <td>{{ item.data[0] }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import BarChart from "@/components/BarChart";
+import Vue from "vue";
+import { DataProvider } from "@/components/data-provider";
+
+const provider = new DataProvider();
+
+export default Vue.extend({
+  components: { BarChart },
+  data() {
+    return {
+      contributors: provider.randomCollection(5),
+      sum: provider.randomCollection(1),
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+      chartStyles: {
+        // position: "relative",
+      },
+    };
+  },
+  methods: {},
+});
+</script>
+
+<style></style>
